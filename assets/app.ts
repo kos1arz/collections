d6b8f7a29aa19df3dac0f57e36aa0515d5b44c4f
przemekdziak <dziakprzemek@gmail.com>
2023-08-17 23:10:57 +0200

    added bootstrap

--- assets/app.ts
+++ assets/app.ts
@@ -7,3 +7,10 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import './styles/main.scss'
+
+import $ from 'jquery';
+
+$(function (){
+   console.log('test');
+})
