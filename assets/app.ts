66b3aa12c1de9d00caac3fe9a15d6a4274862cd1
marcin.stamm <marcin.stamm@crafton.pl>
2023-09-11 18:30:44 +0200

    Bootstrap 5 tabs

--- assets/app.ts
+++ assets/app.ts
@@ -6,35 +6,37 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/font.css';
-import './styles/app.css';
-import './styles/main.scss';
+import "./styles/main.scss";
+import "./styles/font.css";
+import "./styles/app.css";
+import "./styles/modules/tabs.scss";
 
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'owl.carousel';
-import 'owl.carousel/dist/assets/owl.carousel.css';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "owl.carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
 
 // const $ = require('jquery');
 // require('bootstrap');
+// $("#recipeCarousel").owlCarousel({
+//   interval: 10000,
+// });
+$(document).ready(function () {
+  // $("#recipeCarousel.carousel .carousel-item").owlCarousel({
+  //     loop: true,
+  //     autoplay: true,
+  //     autoplayTimeout: 10000,
+  // });
+
   // $("#recipeCarousel").owlCarousel({
   //   interval: 10000,
   // });
-$(document).ready(function() {
 
-
-    // $("#recipeCarousel.carousel .carousel-item").owlCarousel({
-    //     loop: true,
-    //     autoplay: true,
-    //     autoplayTimeout: 10000,
-    // });
-
-
-  // $("#recipeCarousel").owlCarousel({
-  //   interval: 10000,
-  // });
-
-  $("#recipeCarousel.carousel .carousel-item").each(function(this: HTMLElement, index: number, element: HTMLElement) {
+  $("#recipeCarousel.carousel .carousel-item").each(function (
+    this: HTMLElement,
+    index: number,
+    element: HTMLElement
+  ) {
     var minPerSlide = 3;
     var next = $(this).next();
     if (!next.length) {
@@ -53,44 +55,35 @@ $(document).ready(function() {
   });
 });
 /////////////////////////////////////////////////////////////////////////////////////////////////
-$(document).ready(function(){
+$(document).ready(function () {
+  if ($(".brands_slider").length) {
+    var brandsSlider = $(".brands_slider");
 
-  if($('.brands_slider').length)
-       {
-           var brandsSlider = $('.brands_slider');
+    brandsSlider.owlCarousel({
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 1000,
+      nav: false,
+      dots: false,
+      autoWidth: true,
+      items: 8,
+      margin: 42,
+    });
 
-           brandsSlider.owlCarousel(
-           {
-               loop:true,
-               autoplay:true,
-               autoplayTimeout:1000,
-               nav:false,
-               dots:false,
-               autoWidth:true,
-               items:8,
-               margin:42
-           });
+    if ($(".brands_prev").length) {
+      var prev = $(".brands_prev");
+      prev.on("click", function () {
+        brandsSlider.trigger("prev.owl.carousel");
+      });
+    }
 
-           if($('.brands_prev').length)
-           {
-               var prev = $('.brands_prev');
-               prev.on('click', function()
-               {
-                   brandsSlider.trigger('prev.owl.carousel');
-               });
-           }
-
-           if($('.brands_next').length)
-           {
-               var next = $('.brands_next');
-               next.on('click', function()
-               {
-                   brandsSlider.trigger('next.owl.carousel');
-               });
-           }
-       }
-
-
+    if ($(".brands_next").length) {
+      var next = $(".brands_next");
+      next.on("click", function () {
+        brandsSlider.trigger("next.owl.carousel");
+      });
+    }
+  }
 });
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
